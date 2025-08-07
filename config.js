417b4889a27a3cb72a44b485d5d75e9b9c008412
Hiddno <badrikatanyamah3@gmail.com>
2025-08-08 02:04:41 +0700

    birthday

---
+++ config.js
@@ -0,0 +1,55 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "kasih dong ke aku",      //Di sini, koma terakhir di akhir setiap kalimat harus dalam bahasa Inggris! Ini sangat penting!
+        "mantan kesayangan mwhehe",  // 同上...
+        "hari ini hari spesial buat kamu",
+        "biar cepet gede",
+        "berarti ini ulang tahun yang ke 16 yeee！",
+        "aku suka kamu yang, ada deh diselanjutnya.",
+        "Penampilan aneh kamu",
+        "Penampilan lucu kamu",
+        "Ekspresi marah kamu",
+        "Itu semua yang aku suka",
+        "ngomong ngomong, udah nerima hadiah nya belom?? kalo belum besok wk？",
+        "makan yang banyak sebanyak banyak nya",
+        "jangan lupa jajan",
+        "supaya cepet gede mwhehe",
+        "sama merindukanku！",
+        "Selamat ulang tahun~~",
+    ],
+    imgs: {
+        "kasih dong ke aku": "./imgs/songxinxin.jpeg",
+        "mantan kesayangan mwhehe": "./imgs/nothing.jpg",
+        "hari ini hari spesial buat kamu": "./imgs/shengri.jpeg",
+        "biar cepet gede": "./imgs/zaiyiqi.png",
+        "berarti ini ulang tahun yang ke 16 yeee！": "./imgs/nothing.jpg",
+        "aku suka kamu yang, ada deh diselanjutnya.": "./imgs/yangzi.jpeg",
+        "Penampilan aneh kamu": "./imgs/nothing.jpg",
+        "Penampilan lucu kamu": "./imgs/nothing.jpg",
+        "Ekspresi marah kamu": "./imgs/nothing.jpg",
+        "itu yang aku suka": "./imgs/nothing.jpg",
+        "ngomong ngomong, udah nerima hadiah nya belom?? kalo belum besok wk？": "./imgs/chuo.gif",
+        "makan yang banyak sebanyak banyak nya": "./imgs/chihaodian.jpeg",
+        "jangan lupa jajan": "./imgs/weibao.jpeg",
+        "supaya cepet gede mwhehe": "./imgs/nothing.jpg",
+        "sama merindukanku！": "./imgs/nothing.jpg",
+        "Selamat ulang tahun~~": "./imgs/nothing.jpg"
+    },
+    
+    desc: {
+        turn_on: "Klik Beb",
+        play: "Tambahin Musik Dulu Ga sih? Klik",
+        bannar_coming: "Tambahin Banner Dulu Gasih",
+        balloons_flying: "Ehh Ga ada balon nya klik ga sih??",
+        cake_fadein: "Eh Gada kue？klik dulu ga sih",
+        light_candle: "Lilin？nyalahin dong, sini klik",
+        wish_message: "Selamat ulang tahun Sayang～ klik lagii TwT",
+        story: "aku punya text nih buat kamu, klik dong"
+    },
+
+    // 结束语
+    loveText: 'Aku Selalu Sayang Sama Kamu~'
+};
